69551a8390ebec50741e8912e4c55c4fc2b328e9
Nisarga Shashidhar <Nisarga.Shashidhar@knipper.com>
2024-07-19 12:05:22 -0400

    Upload is handled by Mainlayout

---
+++ app/SharedWebComponents/Models/UploadDocumentsArgs.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedWebComponents.Models;
+public class UploadDocumentsArgs
+{
+    public IReadOnlyList<IBrowserFile> Files { get; set; }
+    public string Category { get; set; }
+    public bool Success { get; set; }
+    public int? UploadedFilesCount { get; set; }
+    public string? ErrorMessage { get; set; }
+}
