e8a26ee67a7c629142c6c6f2bf992a989b22cdb9
Nisarga Shashidhar <Nisarga.Shashidhar@knipper.com>
2024-08-06 14:14:45 -0400

    Updated ChatInstance to have Name

--- app/SharedWebComponents/Models/ChatInstance.cs
+++ app/SharedWebComponents/Models/ChatInstance.cs
@@ -4,6 +4,7 @@ namespace SharedWebComponents.Models;
 
 public class ChatInstance
 {
+    public string Name { get; set; } = "New Chat #"; // Name of the chat
     public Guid Id { get; set; } = Guid.NewGuid(); // Unique identifier for each chat
     public Dictionary<UserQuestion, ChatAppResponseOrError?> ChatHistory { get; set; } = new Dictionary<UserQuestion, ChatAppResponseOrError?>();
 }
